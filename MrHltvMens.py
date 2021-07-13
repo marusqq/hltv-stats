5be9ffacf1ec02e0010096adc2b5a25d9b1d2ebb
Marius Pozniakovas <mpozniakovas@nymbus.com>
2021-07-13 15:53:05 +0300

    changing user-agent

--- MrHltvMens.py
+++ MrHltvMens.py
@@ -12,7 +12,7 @@ from bs4 import BeautifulSoup
 class MrHltvMens:
 
     def __init__(self):
-        self.user_agent = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393'}
+        self.user_agent = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'}
         self.referer = "https://www.google.com/"
         self.url = "https://www.hltv.org/"
 
